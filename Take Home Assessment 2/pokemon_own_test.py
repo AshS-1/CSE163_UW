d2b83f819b0c4a9d9e0c386980599357a12d73a4
AshS-1 <108997371+AshS-1@users.noreply.github.com>
2023-08-06 21:16:44 -0700

    Create pokemon_own_test.py

---
+++ Take Home Assessment 2/pokemon_own_test.py
@@ -0,0 +1,7 @@
+id,name,level,personality,type,weakness,atk,def,hp,stage
+42, Bulbasaur, 24, nice, water, fire, 20, 43, 12, 1
+42, Bulbasaur, 99, mean, fire, fire, 101, 200, 44, 3
+17, Arcanine, 84, mean, electric, fire, 24, 45, 65, 2
+17, Arcanine, 78, horrible, fire, fire, 50, 98, 12, 2
+17, Arcanine, 67, horrible, fire, water, 32, 67, 34, 1
+156, Laprase, 58, nice, electric, water, 32, 57, 99, 3
